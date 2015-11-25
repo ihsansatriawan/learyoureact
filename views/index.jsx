7c56fd0a84459c07a5b1ff638a9f7c802c3729c9
Ihsan Satriawan <ihsan.satriawan.20@gmail.com>
2015-11-25 11:34:29 +0700

    Exercise 5 done

--- views/index.jsx
+++ views/index.jsx
@@ -28,9 +28,22 @@ class TodoList extends React.Component {
 }
 
 class Todo extends React.Component {
+	constructor(props) {
+		super(props);
+		this.state = {checked: false};
+	}
+
+	handlechange(e) {
+		this.setState({checked: e.target.checked});
+	}
+
+
   render() {
   	return (
   		<tr>
+  			<td style={{border: "1px solid black"}}>
+  				<input type="checkbox" checked={this.state.checked} onChange={this.handlechange} />
+  			</td>
         <td style={{border: "1px solid black"}}>{this.props.title}</td>
         <td style={{border: "1px solid black"}}>{this.props.children}</td>
       </tr>
